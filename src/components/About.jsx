ed35f8d552b067908ae07502d24979b0e184e399
Tamir Alam <tamiralam2@gmai.com>
2024-09-27 19:58:42 +0530

    file is added

---
+++ src/components/About.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaReact } from "react-icons/fa";
+import { FaJava } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <h1 className="text-4xl font-bold mb-5 text-green-500 flex justify-center underline">About</h1>
+      <div className="flex flex-col md:flex-row justify-between">
+        {/* Left Side - About Content */}
+        <div className="md:w-1/2 mb-8">
+          <p className="text-lg mb-4">
+            Hello, I'm Tamir, a passionate web developer with a keen eye for
+            the MERN Stack. With a background in IT, I strive to create
+            impactful and visually stunning software solutions that leave a
+            lasting impression.
+          </p>
+          <h2 className="text-green-600 font-semibold text-xl">
+            Education & Training
+          </h2>
+          <span>
+            Degree/Certification || Institution, [Year] <br />
+            Degree/Certification || Institution, [Year] <br />
+            Relevant Course || Platform/Institution, [Year]
+          </span>
+          <br />
+          <br />
+          <h2 className="text-green-600 font-semibold text-xl">
+            Skills & Expertise
+          </h2>
+          <span>
+            Proficient in [Programming Languages]. <br />
+            Experienced with [Software Tools/Technologies]. <br />
+            Strong grasp of [Design Principles/Concepts]. <br />
+            Excellent problem-solving skills. <br />
+            Effective communicator and collaborator.
+          </span>
+          <br />
+          <br />
+          <h2 className="text-green-600 font-semibold text-xl">
+            Mission Statement
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation.
+          </p>
+        </div>
+
+        {/* Right Side - Technologies Section */}
+        <div className="md:w-1/2 flex flex-col justify-center items-center">
+          <h1 className="text-2xl font-bold mb-5 text-center">
+            Technologies I Work With
+          </h1>
+          <div className="flex justify-center items-center gap-8 text-5xl text-gray-700">
+            <FaHtml5 className="text-orange-600" />
+            <FaCss3Alt className="text-blue-500" />
+            <IoLogoJavascript className="text-yellow-500" />
+            <FaReact className="text-blue-400" />
+            <FaJava className="text-red-600" />
+            <FaGithub className="text-green-700"/>
+            <RiTailwindCssFill className="text-blue-400" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Project.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import Reactjs from "../assets/logo192.png";
+import javascript from "../assets/javascript.png";
+import reactjs from "../assets/reactjs.png";
+
+function Project() {
+  const cardItem = [
+    {
+      id: 1,
+      logo:javascript,
+      name:"javascript",
+      // Unique video link
+      sourceCodeLink: "https://exquisite-gumdrop-180e30.netlify.app/", // Unique link for MongoDB project
+      // Unique live demo link
+    },
+    {
+      id:2,
+      logo:reactjs,
+      name:"ReactJS",
+      // Unique video link
+      sourceCodeLink: "http://localhost:3004/", // Unique link for ReactJS project
+      // Unique live demo link
+    },
+    {
+      id: 3,
+      logo: Reactjs,
+      name: "logo192.png",
+      sourceCodeLink: "https://food-project-youi.vercel.app//", // Unique link for Java project
+      // Unique live demo lin
+    },
+  ];
+
+  return (
+    <div
+      name="Project"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 mt-10"
+    >
+      <div>
+        <h1 className="text-4xl font-bold mb-5 text-center text-green-500">Project</h1>
+        <span className="underline font-semibold text-center block mb-8">
+          Recent Projects
+        </span>
+
+        {/* Grid for the cards */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 justify-center my-5">
+          {cardItem.map(
+            ({ id, logo, name, videoLink, sourceCodeLink, liveDemoLink }) => (
+              <div
+                className="md:w-[300px] md:h-[340px] border-[2px] rounded-lg shadow-lg p-5 cursor-pointer hover:scale-110 duration-300"
+                key={id}
+              >
+                <img
+                  src={logo}
+                  className="w-[120px] h-[120px] p-1 rounded-full border-[2px] mx-auto mb-4"
+                  alt={name}
+                />
+                <div>
+                  <div className="text-center font-bold text-xl mb-2">
+                    {name}
+                  </div>
+                  <p className="text-center text-black-700">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                </div>
+                {/* Buttons section with 3 buttons */}
+                <div className="flex justify-center space-x-4 mt-4">
+                  {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded mb-0"> */}
+                  {/* <a href={sourceCodeLink} target="_blank" rel="noopener noreferrer"> */}
+                  {/* Video */}
+                  {/* </a> */}
+                  {/* </button> */}
+                  <button className="bg-green-500 hover:bg-green-700 text-white font-bold px-4 py-2 rounded mb-0">
+                    <a
+                      href={sourceCodeLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Demo
+                    </a>
+                  </button>
+                  {/* <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold px-4 py-2 rounded mb-0"> */}
+                  {/* <a href={sourceCodeLink} target="_blank" rel="noopener noreferrer"> */}
+                  {/* Source Code */}
+                  {/* </a> */}
+                  {/* </button> */}
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
